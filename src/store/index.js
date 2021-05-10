6c54527d9ebdfce943972729dbf7ceb0c94448ce
squirrelRao <hqraop@163. com>
2021-05-10 21:46:29 +0800

    add store some codes

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,63 @@
+import axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
-   
+     user_id:'',
+     name:''
   },
   mutations: {
+
+    login_callback(data){
+      state.user_id = data["user_id"];
+      state.name = data["name"];
+    }
     
   },
+  getters:{
+    isLogin: user_id == '' ? false : true    
+  },
   actions: {
-    // login(context, user) {
-    //   context.commit("login", user);
-    // }
+   
+    login(context, phone,code) {
+
+      axios.post("/v1/login",{"phone":phone,"code":code}).then(res => {
+        console.log(res);
+        this.$message({
+          showClose: true,
+          message: res["data"],
+          type: "success"
+        });
+        if(res["rc"] == 0){
+
+          expire = new Date().getTime() + 3688 * 24 * 10;
+          localStorage.setItem("user",JSON.stringify({"user_id":res["data"]["user_id"],"name":res["data"]["name"],"expire":expire}));
+          context.commit("login_callback", res["data"]);
+        
+        }else{
+          this.$message({
+            showClose: true,
+            message: res["msg"],
+            type: "error"
+          });
+        }
+
+      }).catch(err => {
+        console.log(err);
+        this.$message({
+          showClose: true,
+          message: "req failure",
+          type: "error"
+        });
+      });
+
+     
+    }
   },
   modules: {}
 });
+
+export default store;
